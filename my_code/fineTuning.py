10a9515b5c9a0a4dd7334858e61541f59da4079c
Konstantinos Kokkalis <kokkaleskonstantinos@gmail.com>
2024-05-01 20:42:08 +0300

    UPDATE: new experiment
    New experiment using fine tuning optuna according to MAPE metric.
    Results are on my_code/optuna-logs/optuna_results_mape

--- my_code/fineTuning.py
+++ my_code/fineTuning.py
@@ -7,10 +7,10 @@ import sys
 from batteryml.pipeline import Pipeline
 import shutil
 
-NUM_OF_TRIALS=100
+NUM_OF_TRIALS=50
 
-if  os.path.exists("./cache"):
-    shutil.rmtree("./cache")
+# if  os.path.exists("./cache"):
+#     shutil.rmtree("./cache")
 
 def objective(trial, model_type, model):
     data = 'hust'
@@ -23,36 +23,31 @@ def objective(trial, model_type, model):
         hyperparameters = {
             # "in_channels": trial.suggest_int("in_channels", 1, 3, step=1),
             "channels": trial.suggest_int("channels", 8, 64, step=8),
-            "epochs": trial.suggest_int("epochs", 100, 1000, step=100),
+            "epochs": trial.suggest_int("epochs", 100, 1500, step=100),
             "batch_size": trial.suggest_int("batch_size", 32, 256, step=32),
             "interp_dim": trial.suggest_int("interp_dim", 600, 2000, step=200),
-            # "input_height": trial.suggest_int("input_height", 500, 10000, step=50),  # Adjust range as needed
-            # "input_width": trial.suggest_int("input_width", 500, 10000, step=50),   # Adjust range as needed
-            # "evaluate_freq": trial.suggest_int("evaluate_freq", 50, 200, step=50),
-            # in_channels: 1
-            # channels: 16
-            # input_height: 100
-            # input_width: 1000
-            # epochs: 1000
-            # batch_size: 128
-            # evaluate_freq: 100
+            # "input_height": trial.suggest_int("input_height",100, 600, step=100),
+            # "input_width": trial.suggest_int("input_width",250, 10000, step=250),
+
         }
-    # config['model']['in_channels']=hyperparameters['in_channels']
     config['model']['channels'] = hyperparameters['channels']
     config['model']['epochs'] = hyperparameters['epochs']
     config['model']['batch_size'] = hyperparameters['batch_size']
     config['feature']['interp_dim'] = hyperparameters['interp_dim']
-    # config['model']['evaluate_freq'] = hyperparameters['evaluate_freq']
+    # config['model']['input_height'] = hyperparameters['input_height']
+    # config['model']['input_width'] = hyperparameters['input_width']
     with open(config_path, 'w') as stream:
         yaml.safe_dump(config, stream)
-
-    pipeline = Pipeline(config_path=config_path, workspace=f'workspaces/cnn')
-    # train_loss , test_loss = pipeline.train()
-    model, dataset = pipeline.train(device='cuda', skip_if_executed=False)
-    train_prediction = model.predict(dataset, data_type='train')
-    train_loss = dataset.evaluate(train_prediction, 'MAPE', data_type='train')
-    test_prediction = model.predict(dataset, data_type='test')
-    test_loss = dataset.evaluate(test_prediction, 'MAPE', data_type='test')
+    try:
+        pipeline = Pipeline(config_path=config_path, workspace=f'workspaces/cnn')
+        # train_loss , test_loss = pipeline.train()
+        model, dataset = pipeline.train(device='cuda', skip_if_executed=False)
+        train_prediction = model.predict(dataset, data_type='train')
+        train_loss = dataset.evaluate(train_prediction, 'MAPE', data_type='train')
+        test_prediction = model.predict(dataset, data_type='test')
+        test_loss = dataset.evaluate(test_prediction, 'MAPE', data_type='test')
+    except:
+        return 10000
 
     # validation_loss = evaluate_model(model, validation_data)  # Replace with your evaluation function
 
@@ -70,7 +65,7 @@ def objective(trial, model_type, model):
 results_dict = {}
 
 # Define a list of model types to iterate over
-nn_models = ['lstm' ,'mlp','cnn']
+nn_models = ['mlp' ,'cnn','lstm']
 model_types=['sklearn','nn_models']
 
 model_type='nn_models'
@@ -90,7 +85,8 @@ for model_type, result in results_dict.items():
     print(f"Model type: {model_type}")
     print(f"Best parameters: {best_params}")
     print(f"Best test loss: {best_test_loss}")
-file_path = "./optuna-logs/nn_model_best_new.txt"
-with open(file_path, 'w') as file:
+file_path = "./optuna-logs/optuna_results.txt"
+with open(file_path, 'a') as file:
     # Write data to the file
+    file.write("\n")
     file.write(str(results_dict))
